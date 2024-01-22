9672ce090e7d26d565f77799975ca5e21c8bc28f
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-22 18:47:14 +0000

    text

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * main - prints a string of characters
- * Description: prints _putchar, followed by a new line
- * Return: 0
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char a[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(a[i]);
-	}
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 } 
