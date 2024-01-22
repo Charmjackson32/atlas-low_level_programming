95e7e3e9115242f280abe5977377475fd5600182
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-22 12:01:04 +0000

    text

---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+
+/**
+ * _putchar -write the character c to stdout
+ * @c: the character to print
+ * return: on success 1.
+ * onerror, -1 is returnted,and errmo is set appropriately.
+ */   in_putchar(char c)
+ 
+{     return (writed(1, &c 1,));
+
+}
