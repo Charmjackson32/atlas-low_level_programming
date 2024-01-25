dbcc1a7db7215cd6ebd6700f650e3b289c457307
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-25 14:22:02 +0000

    text

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Vimmer the bunny is cute lil bunny
- *
- * Return: Always 0.
+ * _isupper - a function that checks for uppercase character
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int main(void)
+int _isupper(int c)
 {
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
