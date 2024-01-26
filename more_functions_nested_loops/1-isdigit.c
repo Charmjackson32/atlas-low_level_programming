024b6362a43381bf7fb895787b58b5e679e7bc9c
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-26 16:57:35 +0000

    check for a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * isdigit- checks to see if char is digit
+ * @c: input integer
+ *
+ * Return: 1 if digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c < 49 && c >= 39)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
