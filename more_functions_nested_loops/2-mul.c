0c08937a240c1d69ceea86bfd7d3610ba06a0606
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-26 17:57:46 +0000

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** 
+ * mul- multiplies
+ * @a: integer
+ * @b:  integer
+ *
+ * Return: return mul
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,11 @@
 int _putchar(char c);
 int _isdigit(int c);
 int _isupper(int c);
-
-#endif /* MAIN_H */
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_diagonal(int n);
+void print_square(int size);
+void FizzBuzz(void) void 
+void print_triangle(int size);
