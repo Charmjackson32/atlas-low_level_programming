3a03046406944225a5726de62b7a0f25ce101d9a
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-28 13:26:37 +0000

    text

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,15 @@
-#include "main.h"
+#include" 8-print_square.c"
 
 /**
  * print_line - makina functions that draws a stright line in the terminal.
  * @n: how many times number of the character'_' would be printed off.
  */
 void print_line(int n)
+
 {
-	if (n <='0')
-	{
-		_putchar('\n');
-		return;
-	}
+	char c;
 
-	if	 (n >='0')
-	{
+	for (c = 0; c < n; c++)
 		_putchar('_');
-		n--;
-	}
-
 	_putchar('\n');
 }
