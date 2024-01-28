873a260541efd96f7e5f0e66b39a799567d91a56
Charmjackson32 <charmjackson2933@gmail.com>
2024-01-28 23:24:32 +0000

    text

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
 /**
- * puts2 - Prints every other character of a string, starting with the first
- * @str: Pointer to the input string
- *
- * Description: This function prints every other character of the input
- * string, starting with the first character, followed by a new line.
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
 void puts2(char *str)
 {
-	unsigned int i = 0;
-	unsigned int limit = 446; /* Adjust this limit as needed */
-
-	while (str[i] != '\0' && i < limit)
+	int i = 0;
+	for (; str[i] != '\0'; i++)
 	{
-		/* Print characters at even indices */
-		_putchar(str[i]);
-		i += 2;
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	}
-
-	/* Print a new line at the end */
 	_putchar('\n');
+}
